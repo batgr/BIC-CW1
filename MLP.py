d80c9bcd293d3c635e46832146e08550811c831e
grevy <grevy.batsotsa@esme.fr>
2025-11-05 12:40:18 +0000

    Add the sigmoid function

--- MLP.py
+++ MLP.py
@@ -11,5 +11,17 @@ class Neuron:
     def output(self):
         z = sum((x*w for x,w in zip(self.inputs,self.weights)),self.bias)
         out = self.activation(z)
-        return
+        return out
 
+
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+
+neuron = Neuron([1.0,2.0,3.0],sigmoid)
+
+print(neuron.inputs)
+print(neuron.n_inputs)
+print(neuron.bias)
+print(neuron.weights)
+print(neuron.output())
