8c36d165d30c9592d2203e3a555aea38649b81d0
grevy <grevy.batsotsa@esme.fr>
2025-11-13 22:07:53 +0000

    Add class Particle

--- PSO.py
+++ PSO.py
@@ -1,10 +1,31 @@
 import numpy as np
 
-class PSO:
-    def __init__(self,swarmsize,a,b,g,l,s,e):
+class Particle:
+    def __init__(self,swarmsize):
+        self.position = np.random.uniform(-swarmsize,swarmsize)
+        self.velocity = np.random.uniform(-swarmsize,swarmsize)
+        self.best_position = self.position
+        self.best_fitness = -np.inf
         
-        self.p = {np.random.uniform(-swarmsize,swarmsize):np.random.uniform(-swarmsize,swarmsize) for _ in range(swarmsize)}
+class PSO:
+    def __init__(self,swarmsize,a,b,g,l,s,e,assess_fitness):
+        
+        self.particles = {Particle(swarmsize) for _ in range(swarmsize)}
         self.best = None
+        self.assess_fitness = assess_fitness
+        self.best_fittest = None
+    
+    def optimize(self):
+        while True:
+            for particle in self.particles:
+                fitness = self.assess_fitness(particle)
+                 
+                if self.best is None or fitness > self.best_fittest:
+                    self.best = particle
+                    particle.
+            
+            for particle in self.particles
+                
         
 
 
