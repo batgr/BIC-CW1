19d6996df2cf81aaf73b6f5042f483d7c0933121
grevy <grevy.batsotsa@esme.fr>
2025-11-14 04:05:29 +0000

    Modify the Ann to combine with PSO

--- ANN_PSO.py
+++ ANN_PSO.py
@@ -1,16 +1,21 @@
 import numpy as np
+from PSO import PSO
 class Neuron:
     
     def __init__(self,n_inputs,activation):
         self.n_inputs = n_inputs
-        self.bias = np.random.uniform(-1,1)
-        self.weights = [np.random.uniform(-1,1) for _ in range(n_inputs)]
+        self.bias = None
+        self.weights = None
         self.activation = activation
 
     def output(self,inputs):
         z = np.dot(inputs,self.weights) + self.bias
         out = self.activation(z)
         return out
+    
+    def set_params(self,weights,bias):
+        self.weights = weights
+        self.bias = bias
 
 class Layer:
     def __init__(self,n_inputs,n_neurons,activation):
@@ -26,6 +31,7 @@ class MLP:
         self.inputs = inputs
         dim = [len(inputs)] + n_layer_nodes
         self.layers = [Layer(n_i,n_o,activation) for n_i,n_o in zip(dim[:-1],dim[1:])]
+        self.n_params = sum((n_i*n_o )+1 for n_i,n_o in zip(dim[:-1],dim[1:]))
     
     def output(self):
         out = self.inputs
@@ -33,17 +39,21 @@ class MLP:
             out = layer.output(out)
         return out
     
+    def set_params(self,params):
+         i = 0
+         for layer in self.layers:
+            for neuron in layer.neurons:
+                n = neuron.n_inputs
+                weights = params[i : i+n]
+                bias = params[i+n]
+                neuron.set_params(weights, bias)
+                i += n + 1
+    
+  
     
     
-    
-            
-
-def sigmoid(z):
-    return 1 / (1 + np.exp(-z))
 
 
-x = [1.0,2.0]
-dim_layer = [2,2]
 
-mlp = MLP(dim_layer,x,sigmoid)
-print(mlp.output())
+
+
